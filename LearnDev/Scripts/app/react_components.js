f7f5db7196d7357509a04de863f5a42fb2130d71
Aman Beg <aman_b@hotmail.com>
2015-11-13 10:25:31 -0500

    Initial commit.

---
+++ LearnDev/Scripts/app/react_components.js
@@ -0,0 +1,9 @@
+﻿
+var ProfileImage = React.createClass({
+    displayName: 'ProfileImage',
+    render: function() {
+        return React.createElement(
+            'div', null 
+            );
+    }
+});
